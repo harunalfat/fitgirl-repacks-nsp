5352b4a728a8789a6574577c6be8b9c9084f72e5
Alfat Saputra Harun <harunalfat@gmail.com>
2022-02-21 02:08:00 +0700

    finished commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# FITGIRL-NSP
+
+This is a tool/executable for Windows to extract [Fitgirl Repack](https://fitgirl-repacks.site/) switch emulated package game into its own NSP. You may need to get standalone NSP file format, maybe to play into your own CFW Switch or another emulator (Ryujinx, etc)
+
+### How to RUN
+- Download the latest `extractor.exe` binary from [release page](https://github.com/harunalfat/fitgirl-nsp)
+- Copy the downloaded `extractor.exe` into the root of switch emulated Fitgirl-repacks directory. Example directory is `"C:/Games/Mario Kart 8 Deluxe"`
+- Double click or run the `extractor.exe` file, and the default name for output file is `out.nsp`. You may also run it from CMD/Powershell and changing the output file name
+```
+extractor.exe -o mario-kart-8-deluxe.nsp
+```
+
+### How to COMPILE
+- You need to have Golang with version >= 1.16
+- Run command below
+```
+GOOS=windows GOARCH=amd64 go build extractor.go
+```
+
+### Dependency
+- [nspBuild](https://github.com/CVFireDragon/nspBuild/releases) by CVFireDragon
+
---
+++ extractor.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	_ "embed"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+//go:embed nspBuild.exe
+var binary []byte
+
+const nspBuildExecutable = "nspBuild.exe"
+
+func extractNcaFilesPath(directoryPath string) []string {
+	filesPath := make([]string, 0)
+	entries, err := os.ReadDir(directoryPath)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			splitted := strings.Split(entry.Name(), ".")
+			if len(splitted) > 1 && strings.ToLower(splitted[len(splitted)-1]) == "nca" {
+				fileName := directoryPath + "/" + entry.Name()
+				fmt.Printf("Fetching file %s\n", entry.Name())
+				filesPath = append(filesPath, fileName)
+			}
+
+			continue
+		}
+
+		dirName := directoryPath + "/" + entry.Name()
+		fmt.Printf("Getting into directory %s\n", dirName)
+		recResults := extractNcaFilesPath(dirName)
+		filesPath = append(filesPath, recResults...)
+	}
+
+	return filesPath
+}
+
+func writeNspBuildExecutable() {
+	err := os.WriteFile(nspBuildExecutable, binary, 0755)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	outputName := flag.String("o", "out.nsp", "NSP output file name. Example: super_smash_bros.nsp")
+	flag.Parse()
+
+	execPath, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	pwd := filepath.Dir(execPath)
+
+	nandRegisteredContentsPath := pwd + "/user/nand/user/Contents/registered"
+	// List of current directory children (files or folders)
+	_, err = os.ReadDir(nandRegisteredContentsPath)
+	if err != nil {
+		panic(errors.New("NAND NCA contents cannot be found. Wrong location or not compatible with Fitgirl-Repacks switch emulated format"))
+	}
+
+	arguments := extractNcaFilesPath(nandRegisteredContentsPath)
+
+	writeNspBuildExecutable()
+	output, err := exec.Command(nspBuildExecutable, append([]string{*outputName}, arguments...)...).Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(output))
+	fmt.Printf("Succeed build file %s\n", *outputName)
+
+}
